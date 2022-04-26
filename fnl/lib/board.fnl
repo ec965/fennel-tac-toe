f915776b912e226e432f25f125f7c85789c348e5
ec965 <22567131+ec965@users.noreply.github.com>
2022-04-26 01:02:54 -0700

    primitive board checking

--- fnl/lib/board.fnl
+++ fnl/lib/board.fnl
@@ -1,8 +1,16 @@
-(fn draw []
-  (print :todo))
-(fn check-row []
-  (print :todo))
-(fn check-col []
-  (print :todo))
+(fn draw [board]
+  (each [i row (ipairs board)]
+    (-> (accumulate [str "" j tile (ipairs row)]
+          (.. str " " tile))
+        (print))))
+
+(fn check-col [board index turn]
+  (accumulate [won true i row (ipairs board)]
+    (let [tile (. row index)]
+      (and won (= turn tile)))))
+
+(fn check-row [board index turn]
+  (accumulate [won true i tile (ipairs (. board index))]
+    (and won (= turn tile))))
 
 {: draw : check-row : check-col}
--- lua/lib/board.lua
+++ lua/lib/board.lua
@@ -1,10 +1,29 @@
-local function draw()
-  return print("todo")
+local function draw(board)
+  for i, row in ipairs(board) do
+    local function _1_()
+      local str = ""
+      for j, tile in ipairs(row) do
+        str = (str .. " " .. tile)
+      end
+      return str
+    end
+    print(_1_())
+  end
+  return nil
 end
-local function check_row()
-  return print("todo")
+local function check_col(board, index, turn)
+  local won = true
+  for i, row in ipairs(board) do
+    local tile = row[index]
+    won = (won and (turn == tile))
+  end
+  return won
 end
-local function check_col()
-  return print("todo")
+local function check_row(board, index, turn)
+  local won = true
+  for i, tile in ipairs(board[index]) do
+    won = (won and (turn == tile))
+  end
+  return won
 end
 return {draw = draw, ["check-row"] = check_row, ["check-col"] = check_col}
